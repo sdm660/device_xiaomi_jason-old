458b6100dc754e17a5a331f26b2c180eeda8e14d
TheStrix <parthbhatia98@gmail.com>
2018-09-11 19:31:33 +0530

    jason: Enable system-as-root
     - Used for non-a/b as well in pie.
     - Configure dm-verity as it's requirement as well.
    
    Change-Id: I23083a97df77a3cb61380184ea1a91671c34703d

--- device.mk
+++ device.mk
@@ -291,6 +291,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.ad.hdr_calib_data=/system/vendor/etc/hdr_config.cfg \
     ro.vendor.display.sensortype=2
 
+# Device mapper verity
+PRODUCT_SYSTEM_VERITY_PARTITION=/dev/block/bootdevice/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION=/dev/block/bootdevice/by-name/cust
+$(call inherit-product, build/target/product/verity.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
