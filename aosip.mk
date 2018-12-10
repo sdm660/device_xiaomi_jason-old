3f3f794c06b978b24b4f131b8f78e2d6c9efec2c
Dusan Uveric <dusan.uveric9@gmail.com>
2018-12-10 17:33:06 +0000

    jason: initial AOSiP
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -21,10 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from jason device
 $(call inherit-product, device/xiaomi/jason/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_jason
+PRODUCT_NAME := aosip_jason
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jason
 PRODUCT_MANUFACTURER := Xiaomi
