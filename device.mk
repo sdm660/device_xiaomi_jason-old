3cf8a33f01b846d5c4e74e0aebd53098002ced05
Dusan Uveric <dusan.uveric9@gmail.com>
2019-02-25 18:25:01 +0000

    Revert "jason: Build Trust HAL"
    This reverts commit 81afab57ed9abb47f65008713f3107a496333a08.

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 # 
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -375,8 +375,7 @@ PRODUCT_PACKAGES += \
 
 # Lineage hardware
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.jason \
-    vendor.lineage.trust@1.0-service
+    vendor.lineage.touch@1.0-service.jason
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
