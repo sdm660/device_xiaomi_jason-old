LineageOS/android_device_xiaomi_jason

jason: Align NFC config with LA.UM.7.2.r1-06500-sdm660.0
Change-Id: If0f9d58cbe5cc0e9b3dddb1df2c6da732d447637

device.mk
@@ -419,7 +419,7 @@ PRODUCT_PACKAGES += \
     nfc_nci.nqx.default.hw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \

sdm660/device_xiaomi_jason-old

device.mk
@@ -418,7 +418,7 @@ PRODUCT_PACKAGES += \
     nfc_nci.nqx.default.hw
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \