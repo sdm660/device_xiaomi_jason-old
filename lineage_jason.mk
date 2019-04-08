b3f74f6c6e3687b13a8620654d2ae2036176dbe4
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    jason: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66
    Signed-off-by: EnesSastim <sastimenes@gmail.com>

--- lineage_jason.mk
+++ lineage_jason.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/jason/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 PRODUCT_NAME := lineage_jason
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jason
