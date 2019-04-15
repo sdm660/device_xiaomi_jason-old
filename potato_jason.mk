28608e7aee68f0557d2a8adf8e5a78d153cdc51c
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    jason: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589
    Signed-off-by: EnesSastim <sastimenes@gmail.com>

--- potato_jason.mk
+++ potato_jason.mk
@@ -24,6 +24,9 @@ $(call inherit-product, device/xiaomi/jason/device.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit some common POSP stuff.
 $(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
