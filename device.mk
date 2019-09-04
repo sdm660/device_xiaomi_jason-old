ca98a2c534260c58344df399b446531584f9815f
Erfan Abdi <erfangplus@gmail.com>
2019-09-04 17:50:41 +0430

    jason: Setup device soong namespace
    Change-Id: I2e9c0ab68a0fe86ba18e3955e2823b87d88e835a
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: EnesSastim <sastimenes@gmail.com>

--- device.mk
+++ device.mk
@@ -524,6 +524,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=3000000 \
     debug.sf.early_gl_app_phase_offset_ns=15000000
 
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    device/xiaomi/jason
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
