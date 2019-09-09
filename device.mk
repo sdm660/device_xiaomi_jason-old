16222c6a4bc6afe05f09563320df243c800fbc51
EnesSastim <sastimenes@gmail.com>
2019-09-09 00:12:14 +0000

    jason: switch to Camera2
    Signed-off-by: EnesSastim <sastimenes@gmail.com>

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
+    Camera2 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.sdm660 \
